84b7972b69b8de5a3897e6a35037cbbd5a6a9a50
Anushka Jaiswal <anushka98jaiswal@gmail.com>
2020-07-14 01:20:40 +0530

    Week two assignment

--- Course2/React/confusion/src/component/MainComponent.js
+++ Course2/React/confusion/src/component/MainComponent.js
@@ -5,6 +5,7 @@ import DishDetail from './DishDetailComponent';
 import {DISHES} from '../shared/dishes';
 import Header from './HeaderComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { COMMENTS } from '../shared/comments';
@@ -47,6 +48,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
+              <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
             </Switch>
--- Course2/React/confusion/src/shared/leaders.js
+++ Course2/React/confusion/src/shared/leaders.js
@@ -2,7 +2,7 @@ export const LEADERS = [
     {
       id: 0,
       name: 'Peter Pan',
-      image: '/assets/images/alberto.png',
+      image: './assets/images/alberto.png',
       designation: 'Chief Epicurious Officer',
       abbr: 'CEO',
       featured: false,
@@ -11,7 +11,7 @@ export const LEADERS = [
     {
       id: 1,
       name: 'Dhanasekaran Witherspoon',
-      image: '/assets/images/alberto.png',
+      image: './assets/images/alberto.png',
       designation: 'Chief Food Officer',
       abbr: 'CFO',
         featured: false,
