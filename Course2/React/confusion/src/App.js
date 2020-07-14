3e3c744df5b7de7ed3cb884d0544ac55ae82c848
Anushka Jaiswal <anushka98jaiswal@gmail.com>
2020-07-14 18:40:39 +0530

    Intro to Redux

--- Course2/React/confusion/src/App.js
+++ Course2/React/confusion/src/App.js
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import Main from './component/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+const store = ConfigureStore();
+
 class App extends Component {
-  
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
