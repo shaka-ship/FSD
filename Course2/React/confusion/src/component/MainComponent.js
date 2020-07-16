479f545bb39914c610cbbd845bf676cfe514fc3a
Anushka Jaiswal <anushka98jaiswal@gmail.com>
2020-07-17 01:52:57 +0530

    React Animations

--- Course2/React/confusion/src/component/MainComponent.js
+++ Course2/React/confusion/src/component/MainComponent.js
@@ -9,7 +9,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { connect } from 'react-redux';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { addComment, fetchDishes,fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchDishes,fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 const mapStateToProps = state => {
    return {
@@ -21,8 +21,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    addComment: (dishId, rating, author, comment) => dispatch
-    (addComment(dishId, rating, author, comment)),
+    postComment: (dishId, rating, author, comment) => dispatch
+    (postComment(dishId, rating, author, comment)),
     fetchDishes: () => {dispatch(fetchDishes())},
     resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
     fetchComments: () => {dispatch(fetchComments())},
@@ -48,7 +48,7 @@ class Main extends Component {
             dishesErrMess={this.props.dishes.errMess}
             comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
             commentsErrMess={this.props.comments.errMess}
-            addComment={this.props.addComment}
+            postComment={this.props.postComment}
             />
       );
     };
