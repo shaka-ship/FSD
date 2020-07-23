79958fb7c2abb442b9d0df2ec573a40eaeb8f0ce
Anushka Jaiswal <28728856+shaka-ship@users.noreply.github.com>
2020-07-24 04:17:19 +0530

    Fix

---
+++ json-server/public/precache-manifest.fe03fc0185103bbb27832c89291ffebc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f4ff7fbc6f91c712da5f1abd420829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86eecce0fc04baf7de61",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "a7005f0a1fd0a4186bac",
+    "url": "/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "86eecce0fc04baf7de61",
+    "url": "/static/js/2.7826382f.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.7826382f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7005f0a1fd0a4186bac",
+    "url": "/static/js/main.f15cb6a8.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
